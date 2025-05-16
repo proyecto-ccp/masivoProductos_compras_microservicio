cf10cb40087deb19e8ccf03728efaaeeea284557
EverUniAndes <eo.martinez2@uniandes.edu.co>
2025-05-15 21:58:38 -0500

    incluir token de acceso jwt

---
+++ Productos.Dominio/ObjetoValor/TokenInfo.cs
@@ -0,0 +1,14 @@
+﻿
+
+using System.Net;
+
+namespace Productos.Dominio.ObjetoValor
+{
+    public class TokenInfo
+    {
+        public string Username { get; set; }
+        public int Resultado { get; set; }
+        public string Mensaje { get; set; }
+        public HttpStatusCode Status { get; set; }
+    }
+}
---
+++ ServicioProducto/Middleware/AutorizadorMiddleware.cs
@@ -0,0 +1,60 @@
+﻿using Productos.Dominio.Puertos.Integraciones;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ServicioProducto.Api.Middleware
+{
+    /// <summary>
+    /// Middleware para validar el token de autorización
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public class AutorizadorMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IServicioUsuariosApi _autorizador;
+
+        /// <summary>
+        /// Constructor del middleware
+        /// </summary>
+        /// <param name="next"></param>
+        /// <param name="autorizador"></param>
+        public AutorizadorMiddleware(RequestDelegate next, IServicioUsuariosApi autorizador)
+        {
+            _next = next;
+            _autorizador = autorizador;
+        }
+        /// <summary>
+        /// Procesa el request y valida el token
+        /// </summary>
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var token = context.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+
+            if (!string.IsNullOrEmpty(token)) 
+            {
+                context.Items["UserId"] = await Validar(token);
+            }
+
+            await _next(context);
+        }
+
+        private async Task<string> Validar(string token)
+        {
+            string usuarioToken = string.Empty;
+            try
+            {
+                var resultado = await _autorizador.ValidarToken(token);
+
+                if (resultado.Status == System.Net.HttpStatusCode.OK) 
+                { 
+                    usuarioToken = resultado.Username;   
+                }
+            }
+            catch 
+            {
+                usuarioToken = string.Empty;
+            }
+
+            return usuarioToken;
+        }
+    }
+}
